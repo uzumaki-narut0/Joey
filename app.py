9bfa74c4d25883cc8fbb4a66b1f600ea862541b3
uzumaki-narut0 <kumar.swapnil170@gmail.com>
2017-01-15 03:59:08 +0530

    hi

--- app.py
+++ app.py
@@ -109,27 +109,14 @@ def makeWebhookResult_editorial(problem_code):
             "payload": {
                 "template_type": "generic",
                 "elements": [{
-                    "title": "First card",
-                    "subtitle": "Element #1 of an hscroll",
+                    "title": "Here is the Editorial",
+                    #"subtitle": "Element #1 of an hscroll",
                     #"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
                     "buttons": [{
                         "type": "web_url",
-                        "url": "https://www.messenger.com",
-                        "title": "web url"
-                    }, {
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for first element in a generic bubble",
-                    }],
-                }, {
-                    "title": "Second card",
-                    "subtitle": "Element #2 of an hscroll",
-                    #"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
-                    }],
+                        "url": speech,
+                        "title": problem_code
+                    }]
                 }]
             }
         }
