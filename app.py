20a8174f55eb7f4b5eb2874e749aba35c4d05549
uzumaki-narut0 <kumar.swapnil170@gmail.com>
2017-01-15 03:48:13 +0530

    hi

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ def makeWebhookResult_editorial(problem_code):
                 "elements": [{
                     "title": "First card",
                     "subtitle": "Element #1 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                    #"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
                     "buttons": [{
                         "type": "web_url",
                         "url": "https://www.messenger.com",
@@ -124,7 +124,7 @@ def makeWebhookResult_editorial(problem_code):
                 }, {
                     "title": "Second card",
                     "subtitle": "Element #2 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                    #"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
                     "buttons": [{
                         "type": "postback",
                         "title": "Postback",
